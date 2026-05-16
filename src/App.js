7ee4e51b1b4a92df6ee1a7c53e3839f529368d5a
Lidio Jr <lidiojunior1234567tn@gmail.com>
2026-05-15 22:23:23 -0300

    feat: adiciona componente Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      {/* Cabeçalho */}
       <NavBar />
       
-      {/* Colocar o resto do conteúdo das páginas*/}
-      <main style={{ padding: '20px' }}>
+      {/* Resto da página*/}
+      <main style={{ padding: '20px', minHeight: '60vh' }}>
         <h1>Plataforma</h1>
       </main>
+
+      {/* Rodapé */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Footer.css';
+import logoFooterImg from '../assets/logo-footer.svg';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      
+      <div className="footer-content">
+        
+        {/* Linha*/}
+        <hr className="footer-line" />
+
+        <div className="footer-bottom">
+          {/* Lado Esquerdo: Links */}
+          <div className="footer-links">
+            <a href="#">Mindemy</a>
+            <a href="#">Ensine na Mindemy</a>
+            <a href="#">Baixe o aplicativo</a>
+            <a href="#">Quem somos</a>
+            <a href="#">Fale conosco</a>
+          </div>
+
+          {/* Lado Direito: Logo */}
+          <div className="footer-logo">
+            <img src={logoFooterImg} alt="Mindemy - Invista no seu sucesso" />
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
